d6a8303e7aa2ea47c20c56641c4009d4e26f6c9c
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-08-05 04:26:17 -0700

    pycodestyle errror fix

--- console.py
+++ console.py
@@ -74,7 +74,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}' \
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -140,7 +140,7 @@ class HBNBCommand(cmd.Cmd):
             else:
                 try:
                     value = float(value)
-                except:
+                except ValueError:
                     if value[0] is '\"' and value[-1] is '\"':
                         valid = 1
                         value = value[1:-1]
@@ -225,7 +225,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -361,6 +361,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
