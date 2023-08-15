43febfd276e8e0bf21698f2e60ff1e67c89e0ef9
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-08-15 04:40:26 -0700

    added task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,42 +1,46 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
-
-
-from flask import Flask
-
+""" Starts a Flash Web Application """
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return "Hello HBNB!"
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """ Prints a Message when /hbnb is called """
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    return "C {}".format(text.replace("_", " "))
+def c_is_fun(text):
+    """ Prints a Message when /c is called """
+    return "C " + text.replace('_', ' ')
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text):
-    return "Python {}".format(text.replace("_", " "))
+def python_is_cool(text='is_cool'):
+    """ Prints a Message when /python is called """
+    return "Python " + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    return "{} is a number".format(n)
+def is_n_number(n):
+    """ Prints a Message when /number is called only if n is an int"""
+    return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
+    """ display a HTML page only if n is an integer """
     return render_template('5-number.html', value=n)
 
 
 if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
