07103391b34a7234087aa43548035fe1c4676cc5
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-08-15 05:59:13 -0700

    state.py

--- models/state.py
+++ models/state.py
@@ -1,45 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
+"""Defines the State class."""
+import models
 from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
+    """Represents a state for a MySQL database.
 
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """returns the list of City instances that belong to the state"""
-            from models import storage
-            cities_dict = storage.all(City)
+            """Get a list of all related City objects."""
             city_list = []
-            for val in cities_dict.values():
-                if self.id == val.state_id:
-                    city_list.append(val)
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
             return city_list
-    else:
-        cities = relationship(
-                "City",
-                back_populates="state",
-                cascade="all, delete, delete-orphan")
-
-    def to_dict(self):
-        """
-        Returns a dict containing all keys/values of __dict__ of the instance
-        """
-        new_dict = self.__dict__.copy()
-        if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
-        new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.strftime(
-            '%Y-%m-%dT%H:%M:%S.%f')
-        new_dict['updated_at'] = self.updated_at.strftime(
-            '%Y-%m-%dT%H:%M:%S.%f')
-        return new_dict
